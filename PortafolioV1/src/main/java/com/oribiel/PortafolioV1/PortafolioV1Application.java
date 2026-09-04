566d8d60154d0f47c3e667828dcdc9f1bc660d12
Oribiel <oribiel14@gmail.com>
2026-09-03 22:33:17 -0700

    Agregar PortafolioV1

---
+++ PortafolioV1/src/main/java/com/oribiel/PortafolioV1/PortafolioV1Application.java
@@ -0,0 +1,13 @@
+package com.oribiel.PortafolioV1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortafolioV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortafolioV1Application.class, args);
+	}
+
+}
---
+++ PortafolioV1/src/test/java/com/oribiel/PortafolioV1/PortafolioV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oribiel.PortafolioV1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortafolioV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
